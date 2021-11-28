f922bace4ca976e876c9906dacd56b12f367c628
songshaoqiu <ssq@qq.com>
2021-11-28 21:09:23 +0800

    first commit

---
+++ src/main/java/com/atguigu/test/GitTest.java
@@ -0,0 +1,17 @@
+package com.atguigu.test;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+
+        /*
+
+        初始化本地库，添加暂存区，提交本地库
+            最上面有一行
+             目的就是在项目下生成.git
+
+         */
+    }
+
+}
