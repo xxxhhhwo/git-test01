f6aed5061182cc7556df2357e586a9359796b265
songshaoqiu <ssq@qq.com>
2021-11-28 21:15:52 +0800

    second commit

--- src/main/java/com/atguigu/test/GitTest.java
+++ src/main/java/com/atguigu/test/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello world");
+        System.out.println("hello ssq");
 
         /*
 
