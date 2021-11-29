ab2a97dde08069f885ca277a55f68a506b46668d
songshaoqiu <ssq@qq.com>
2021-11-29 10:40:29 +0800

    master commit01

--- src/main/java/com/atguigu/test/GitTest.java
+++ src/main/java/com/atguigu/test/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("hello git02");
         System.out.println("master test");
         System.out.println("hot fix test");
+        System.out.println("2021.11.29.10:39");
 
 
         /*
